6c1f42af1f8e89179828f7d71b089844a04c81d3
zekiahmetbayar <zekiahmetbayar1@gmail.com>
2023-10-03 16:11:33 +0300

    feature: List, get and delete templates

--- main.py
+++ main.py
@@ -14,20 +14,49 @@ def CreatePDFReport(body: ReportCreateRequest):
     RenderClass.render(body)
     return FileResponse(Converter.docx2pdf(body.TemplateID))
 
+@app.get("/templates",summary="Lists all templates.", tags=["Template"])
+def ListTemplates():
+    base_path = os.getcwd() + "/templates/"
+    result = []
 
-@app.post("/templates", summary="")
+    for file in [f for f in os.listdir(base_path)]:
+        res = dict()
+        file_stats = os.stat(base_path+ file)
+        res["name"] = file
+        res["size"] = file_stats.st_size
+        result.append(res)
+
+    return result
+
+@app.get("/templates/{name}",summary="Lists all templates.", tags=["Template"])
+def ListTemplates(name: str):
+    base_path = os.getcwd() + "/templates/" + name
+    file_stats = os.stat(base_path)
+    res = dict()
+    res["name"] = name
+    res["size"] = file_stats.st_size
+    
+    return res
+
+@app.delete("/templates/{name}",summary="Lists all templates.", tags=["Template"])
+def ListTemplates(name: str):
+    os.remove(os.getcwd() + "/templates/" + name)
+    return "Item deleted successfully."
+
+
+@app.post("/templates", summary="Uploads new template.", tags=["Template"])
 def SaveTemplate(file: UploadFile = File(...)):
     try:
-        template_id = uuid.uuid4()
         contents = file.file.read()
-        str_template_id = str(template_id)
-        
-        with open(str_template_id, 'wb') as f:
+        name = file.filename.replace(" ", "_")
+        name = file.filename.replace("-", "_")
+       
+        with open(name, 'wb') as f:
             f.write(contents)
     except Exception:
         return {"message": "There was an error uploading the file"}
     finally:
         file.file.close()
-        os.rename(str_template_id, "./templates/" + str_template_id)
+        os.rename(name, "./templates/" + name)
     
-    return {"message": f"Successfully uploaded {file.filename}"}
+    return {"message": f"Successfully uploaded {name}"}
