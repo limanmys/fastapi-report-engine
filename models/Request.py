ffb5e3e7a6d264e3c33731e73929d62701209dcc
zekiahmetbayar <zekiahmetbayar1@gmail.com>
2023-10-23 16:07:35 +0300

    fix: Python version issue

--- models/Request.py
+++ models/Request.py
@@ -2,8 +2,8 @@ from pydantic import BaseModel, Field
 import typing
 
 class ReportCreateRequest(BaseModel):
-    Data: list[typing.Dict[str,typing.Any]] = Field(default_factory=list, alias="data")
-    Columns: list[str] = Field(default_factory=list, alias="columns")
+    Data: typing.List[typing.Dict[str,typing.Any]] = Field(default_factory=list, alias="data")
+    Columns: typing.List[str] = Field(default_factory=list, alias="columns")
     Header: str = Field(alias="header")
     Date: str = Field(alias="date")
     TemplateID: str = Field(alias="template_id")
