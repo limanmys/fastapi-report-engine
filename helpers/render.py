abe7936a39f97957428073723e6e8e4d92ba3898
zekiahmetbayar <zekiahmetbayar1@gmail.com>
2023-10-23 14:12:51 +0300

    feature: Added special key, 'idx'

--- helpers/render.py
+++ helpers/render.py
@@ -3,5 +3,8 @@ from docxtpl import DocxTemplate
 class RenderClass():
     def render(body):
         template = DocxTemplate("./templates/%s" % body.TemplateID)
+        for idx,item in enumerate(body.Data):
+            item["idx"] = idx
+            body.Data[idx] = item
         template.render(body)    
         template.save("./results/%s.docx" % body.TemplateID)
