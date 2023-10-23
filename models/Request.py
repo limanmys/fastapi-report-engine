3b1c0d0a52a44b15f6ee13e55cdb764322c57890
zekiahmetbayar <zekiahmetbayar1@gmail.com>
2023-10-23 16:05:32 +0300

    fix: Nested import

--- models/Request.py
+++ models/Request.py
@@ -1,9 +1,8 @@
 from pydantic import BaseModel, Field
-from typing import Dict
 import typing
 
 class ReportCreateRequest(BaseModel):
-    Data: list[Dict[str,typing.Any]] = Field(default_factory=list, alias="data")
+    Data: list[typing.Dict[str,typing.Any]] = Field(default_factory=list, alias="data")
     Columns: list[str] = Field(default_factory=list, alias="columns")
     Header: str = Field(alias="header")
     Date: str = Field(alias="date")
