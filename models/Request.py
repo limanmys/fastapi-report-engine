84d647645699f13deef0ff70080d6b77fbe68ccb
zekiahmetbayar <zekiahmetbayar1@gmail.com>
2023-10-24 11:24:54 +0300

    feature: Readable Columns

--- models/Request.py
+++ models/Request.py
@@ -4,6 +4,7 @@ import typing
 class ReportCreateRequest(BaseModel):
     Data: typing.List[typing.Dict[str,typing.Any]] = Field(default_factory=list, alias="data")
     Columns: typing.List[str] = Field(default_factory=list, alias="columns")
+    ReadableColumns: typing.List[str] = Field(default_factory=list, alias="readable_columns")
     Header: str = Field(alias="header")
     Date: str = Field(alias="date")
     TemplateID: str = Field(alias="template_id")
