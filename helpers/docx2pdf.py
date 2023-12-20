1ce64c082433df28062fe9c8515e81d943ddcc2c
fthrslntgy <fatiharslantugay@gmail.com>
2023-12-20 16:25:02 +0300

    Fix idiotic confusion

--- helpers/docx2pdf.py
+++ helpers/docx2pdf.py
@@ -2,7 +2,7 @@ import os
 
 class Converter():
     def docx2pdf(file_name):
-        docx_file = "./results/%d.csv" % file_name
+        docx_file = "./results/%d.docx" % file_name
         os.system('soffice --headless --norestore --writer --convert-to pdf ./%s --outdir %s' % (docx_file, "./reports"))
         os.remove(docx_file)
 
